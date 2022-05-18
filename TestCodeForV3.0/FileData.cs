575f722886c9894a488cc261d290339124d9d84e
SSstupid <rlawoghks3@gmail.com>
2022-05-18 17:09:11 +0900

    Upload
    Test: item Window이미지
    
    Item의 WIndow 이미지 확인 완료
    현재 "파일 앨범 이미지 가져오기" 테스트 중

---
+++ TestCodeForV3.0/FileData.cs
@@ -0,0 +1,9 @@
+﻿namespace TestListTemplate
+{
+    public class FileData
+    {
+        public string FileName { get; set; }
+        public string FullPath { get; set; }
+        public System.Windows.Media.ImageSource ff { get; set; }
+    }
+}
---
+++ TestCodeForV3.0/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace TestListTemplate
+{
+    public class MainViewModel
+    {
+        public List<FileData> Files { get; set; }
+        public string Name { get; set; } = "james";
+
+
+        public MainViewModel()
+        {
+            List<FileData> fileDatas = new();
+            fileDatas.Add(new FileData { FileName = "RefCount.txt", FullPath = @"C:\Program Files (x86)\Common Files" ,ff = getIcon() });
+            fileDatas.Add(new FileData { FileName = "Adobe Desktop Service.exe", FullPath = @"C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\ADS" });
+            Files = fileDatas;
+        }
+
+        public System.Windows.Media.ImageSource getIcon()
+        {
+            string filename = @"C:\Users\rlawo\Desktop\TestImg.jpg";
+            System.Windows.Media.ImageSource icon;
+            using (System.Drawing.Icon sysicon = System.Drawing.Icon.ExtractAssociatedIcon(filename))
+            {
+                icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                            sysicon.Handle,
+                            System.Windows.Int32Rect.Empty,
+                            System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            }
+            return icon;
+        }
+    }
+}
