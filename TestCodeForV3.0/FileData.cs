be88c1dab8fcb9d8419f0beac138a155a0a81e42
SSstupid <rlawoghks3@gmail.com>
2022-05-18 19:17:57 +0900

    Upload
    test: 파일의 앨범 이미지 테스트 완료
    
    해당 파일에 포함된 앨범 이미지 가져오기 성공
    다음 ListViewItem에 이미지 추가 완료

--- TestCodeForV3.0/FileData.cs
+++ TestCodeForV3.0/FileData.cs
@@ -1,9 +1,11 @@
-﻿namespace TestListTemplate
+﻿using System.Windows.Media.Imaging;
+
+namespace TestListTemplate
 {
     public class FileData
     {
         public string FileName { get; set; }
         public string FullPath { get; set; }
-        public System.Windows.Media.ImageSource ff { get; set; }
+        public BitmapImage image1 { get; set; }
     }
 }
--- TestCodeForV3.0/MainViewModel.cs
+++ TestCodeForV3.0/MainViewModel.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace TestListTemplate
 {
     public class MainViewModel
     {
         public List<FileData> Files { get; set; }
-        public string Name { get; set; } = "james";
-
 
         public MainViewModel()
         {
             List<FileData> fileDatas = new();
-            fileDatas.Add(new FileData { FileName = "RefCount.txt", FullPath = @"C:\Program Files (x86)\Common Files" ,ff = getIcon() });
+            fileDatas.Add(new FileData { FileName = "RefCount.txt", FullPath = @"C:\Program Files (x86)\Common Files" ,image1 = TestImageMetaData() });
             fileDatas.Add(new FileData { FileName = "Adobe Desktop Service.exe", FullPath = @"C:\Program Files (x86)\Common Files\Adobe\Adobe Desktop Common\ADS" });
             Files = fileDatas;
+        }
+
+        public BitmapImage TestImageMetaData()
+        {
+            //string filename = @"C:\Users\rlawo\Desktop\AllofMe.mp3";
+            string filename = @"C:\Users\rlawo\Desktop\perfect-edsheran.mp3";
+
+            TagLib.File tagFile = TagLib.File.Create(filename);
+
+            if (tagFile.Tag.Pictures.Length < 1) // 여러 아이템 동시 추가시 Length 확인할 것
+            {
+                return null;
+            }
+
+            var imageSource = new BitmapImage();
+            MemoryStream ms = new MemoryStream(tagFile.Tag.Pictures[0].Data.Data);
+            imageSource.BeginInit();
+            imageSource.StreamSource = ms;
+            imageSource.EndInit();
+
+            return imageSource;
         }
 
         public System.Windows.Media.ImageSource getIcon()
