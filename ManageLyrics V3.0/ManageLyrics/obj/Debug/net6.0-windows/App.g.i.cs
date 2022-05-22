a4e194a05c292bf76d802db88f1704f7f8ef235e
SSstupid <rlawoghks3@gmail.com>
2022-05-22 12:13:56 +0900

    .

--- ManageLyrics V3.0/ManageLyrics/obj/Debug/net6.0-windows/App.g.i.cs
+++ ManageLyrics V3.0/ManageLyrics/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace ManageLyrics {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ManageLyrics;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ManageLyrics;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
